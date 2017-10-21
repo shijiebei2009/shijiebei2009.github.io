725e7f2a29add482bbe107bdeae515cc1b7181d9
WangXu <291699763@qq.com>
2017-10-21 22:16:44 +0800

    Site updated: 2017-10-21 22:16:37

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,17 +1,57 @@
 $(document).ready(function () {
-  var $sidebar = $('#sidebar');
-  var headerHeight = $('.header-inner').height();
-  var footerOffset = parseInt($('.main').css('padding-bottom'), 10);
-  var sidebarTop = headerHeight + 10;
 
-  $sidebar
-    .css({ 'margin-top': sidebarTop })
-    .show();
+  var sidebarInner = $('.sidebar-inner');
 
-  NexT.utils.isDesktop() && $('.page-post-detail .sidebar-inner').affix({
-    offset: {
-      top: sidebarTop,
-      bottom: footerOffset
+  initAffix();
+  resizeListener();
+
+  function initAffix () {
+    var headerOffset = getHeaderOffset(),
+        footerOffset = getFooterOffset(),
+        sidebarHeight = $('#sidebar').height() + NexT.utils.getSidebarb2tHeight(),
+        contentHeight = $('#content').height();
+
+    // Not affix if sidebar taller then content (to prevent bottom jumping).
+    if (headerOffset + sidebarHeight < contentHeight) {
+      sidebarInner.affix({
+        offset: {
+          top: headerOffset - CONFIG.sidebar.offset,
+          bottom: footerOffset
+        }
+      });
     }
-  });
+
+    setSidebarMarginTop(headerOffset).css({ 'margin-left': 'initial' });
+  }
+
+  function resizeListener () {
+    var mql = window.matchMedia('(min-width: 991px)');
+    mql.addListener(function(e){
+      if(e.matches){
+        recalculateAffixPosition();
+      }
+    });
+  }
+
+  function getHeaderOffset () {
+    return $('.header-inner').height() + CONFIG.sidebar.offset;
+  }
+
+  function getFooterOffset () {
+    var footerInner = $('.footer-inner'),
+        footerMargin = footerInner.outerHeight(true) - footerInner.outerHeight(),
+        footerOffset = footerInner.outerHeight(true) + footerMargin;
+    return footerOffset;
+  }
+
+  function setSidebarMarginTop (headerOffset) {
+    return $('#sidebar').css({ 'margin-top': headerOffset });
+  }
+
+  function recalculateAffixPosition () {
+    $(window).off('.affix');
+    sidebarInner.removeData('bs.affix').removeClass('affix affix-top affix-bottom');
+    initAffix();
+  }
+
 });
